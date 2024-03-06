d1d0b73ddaa49ef8d323e0e7aef4034f25d4fa00
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2024-03-06 16:31:38 +0700

    adjust tracer calls

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -82,11 +82,11 @@ def index():
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
         # TODO: use tracer object to trace cat vote
-        with tracer.span(name="Cat Vote"):
+        with tracer.span(name="Cat Vote") as span:
             print('Voted for cat')
         vote2 = r.get(button2).decode('utf-8')
         # TODO: use tracer object to trace dog vote
-        with tracer.span(name="Dog vote"):
+        with tracer.span(name="Dog vote") as span:
             print('Voted for dog')
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
